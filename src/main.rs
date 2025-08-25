d670c47b47d66b0628b9572eed378271bf7d644a
zafkiel45 <zafkielc@gmail.com>
2025-08-25 11:08:38 -0300

    feat: added pockets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 use std::process;
 
@@ -5,6 +6,10 @@ struct UrlProfile {
     path: String,
     domain: String,
     category: String, 
+}
+
+struct UrlPocket {
+    urls: HashMap<String, Vec<UrlProfile>>
 }
 
 impl UrlProfile {
@@ -17,49 +22,65 @@ impl UrlProfile {
 }
 
 fn main() {
-    let mut action: String = String::new();
-    let mut url: UrlProfile = UrlProfile { 
-        path: String::new(), 
-        domain: String::new(), 
-        category: String::new(),
-    };
-
+    let mut pocket: UrlPocket = UrlPocket { urls: HashMap::new() };
+    
     loop {
-        let mut category: String = String::new();
-        let mut domain: String = String::new();
-        let mut path: String = String::new(); 
+        let mut pocket_name: String = String::new();
+        let mut action: String = String::new();
 
-        println!("=> Please, type the domain of url");
-        io::stdin().read_line(&mut domain).expect("Invalid input");
+        println!("=> Type some of these commands: add, exit or show-all");
+
+        io::stdin().read_line(&mut action).expect("Invalid Input");
         
-        println!("=> Please, type the path of url");
-        io::stdin().read_line(&mut path).expect("Invalid Input");
+        if action.trim().is_empty() {
+            println!("Bad input");
+            process::exit(1);
+        }
 
-        println!("=> Please, type the category of url");
-        io::stdin().read_line(&mut category).expect("Invalid Input");
+        if action.trim() == "exit" {
+            println!("Quiting of program");
+            process::exit(0);
+        };
 
-        url.category = category.trim().to_string();
-        url.domain = domain.trim().to_string();
-        url.path = path.trim().to_string();
+        if action.trim() == "add" {
+            let mut category: String = String::new();
+            let mut domain: String = String::new();
+            let mut path: String = String::new(); 
+            let mut url: UrlProfile = UrlProfile { 
+                path: String::new(), 
+                domain: String::new(), 
+                category: String::new(),
+            };
+    
+            println!("=> Please, type the domain of url");
+            io::stdin().read_line(&mut domain).expect("Invalid input");
+            
+            println!("=> Please, type the path of url");
+            io::stdin().read_line(&mut path).expect("Invalid Input");
+    
+            println!("=> Please, type the category of url");
+            io::stdin().read_line(&mut category).expect("Invalid Input");
+    
+            println!("=> Please, add to a pocket");
+            io::stdin().read_line(&mut pocket_name).expect("Invalid Input");
+    
+            url.category = category.trim().to_string();
+            url.domain = domain.trim().to_string();
+            url.path = path.trim().to_string();
+    
+            pocket.urls.entry(pocket_name.trim().to_string()).or_default().push(url);
 
-        break;
+            continue;
+        };
+
+        if action.trim() == "show-all" {
+            for (_k, p) in &pocket.urls {
+                for u in p {
+                    u.show_profile();
+                };
+            };
+        };
+
+        continue;
     };
-
-    io::stdin().read_line(&mut action).expect("Invalid Input");
-
-    if action.trim().is_empty() {
-        println!("Bad input");
-        process::exit(1);
-    }
-
-    if action.trim() == "exit" {
-        println!("Quiting of program");
-        process::exit(0);
-    };
-
-    if action.trim() == "show" {
-        url.show_profile();
-    };
-
-
 }
